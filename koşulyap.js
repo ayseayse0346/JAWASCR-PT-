b4d8c0d8ec1fdeb795ef214cdc3aaba65d62f8db
ayseayse0346 <ayseayse.ayse0346@gmail.com>
2024-01-25 17:06:29 +0300

    add

--- koşulyap.js
+++ koşulyap.js
@@ -22,3 +22,18 @@
 
 // ? örnek2:
 
+// DERS ORTLAMASI BULMA
+// vize1,vize2, %30'ar vize etkileyecek, % 40 final etkileyecek
+
+let vize1 = Number(prompt("vize1 notunu "))
+let vize2 = Number(prompt("vize2 notunuzu giriniz."))
+let final = Number(prompt("final notunuzu giriniz"))
+let ortalama = (vize1*0.3)+(vize2*0.3)+(final*0.4)
+if (ortalama>=60 ){
+    alert("geçtiniz tebrikler..."+ortalama)
+    console.log("geçtiniz tebrikler..."+ortalama);
+}
+else{
+    alert("kaldınız..."+ortalama)
+    console.log("kaldınız..."+ortalama);
+}
