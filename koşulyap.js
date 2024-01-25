2e5adb41b9ff202c482bf40b79e2e2a4c0340a7d
ayseayse0346 <ayseayse.ayse0346@gmail.com>
2024-01-25 15:52:23 +0300

    add

---
+++ koşulyap.js
@@ -0,0 +1,24 @@
+// KOŞUL YAPILARI
+
+// let not = 65
+// if ( not >50){
+//     console.log("geçtiniz,notunuz:" +not );
+// }
+// else{
+//     console.log("kaldınız,notunuz:"+not);
+// }
+
+// ? örnek:
+
+// let yaş = Number(prompt("yasınız:"))
+// let para =Number (prompt("bütçeniz"))
+
+// if (yaş>=18 && para>=3000 ){
+//     alert("ehliyet sınavına katılabilirsiniz");
+// }
+// else{
+//     alert("ehliyet sınavına katılmazsınız!");
+// }
+
+// ? örnek2:
+
