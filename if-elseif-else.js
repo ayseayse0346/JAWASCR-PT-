884f9aedd027842eba52739c10597577e4326984
ayseayse0346 <ayseayse.ayse0346@gmail.com>
2024-01-25 17:21:27 +0300

    add

---
+++ if-elseif-else.js
@@ -0,0 +1,31 @@
+//ELSE İF YAPISI
+
+/*
+-              -                  -
+-              -                 -
+  -            -                - 
+    -          -               -
+      -        -              - 
+       -       -             -   
+      1.yol    2.yol        3.yol
+
+
+?kullanıcının dört seçeneği vr o yüzden if else kullanaırız.
+
+
+*/
+
+let seçilenyol = prompt("lütfen gitmek isteiğiniz yolu seçin")
+
+if (seçilenyol === "1"){
+    alert("seçilen yol :"+ seçilenyol+ ". yoldur.")
+}
+else if(seçilenyol==="2"){
+    alert("seçilen yol :"+ seçilenyol+ ". yoldur.")
+}
+else if (seçilenyol==="3"){
+    alert("seçilen yol :"+ seçilenyol+ ". yoldur.")
+}
+else{
+    alert("lütfen yolunuzu seçiniz.")
+}
