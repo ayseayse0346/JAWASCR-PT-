aaceec5624007df4601bb783c3e226685722ac1b
ayseayse0346 <ayseayse.ayse0346@gmail.com>
2024-01-25 15:28:17 +0300

    add

---
+++ diyalog.js
@@ -0,0 +1,22 @@
+//-------------DİYALOG KUTULARI-------//
+/*
+-ALERT
+-PROMPT
+-CONFİRM 
+ */
+
+
+// alert("nasılsın..")
+// alert("işleminize devam etmeden önce kaydetmelisiniz!")
+
+// kullanıcıdan değer alma metodu prompt .kullanıcıdan alınan değerler her zaman stringtir.string döner. eğer number tipi olsun istersem başına number yazarak olur.
+
+// let isim = prompt("isminizi giriniz:")
+// console.log(isim);
+
+// let yas = prompt("yaşınızı giriniz:")
+// console.log(yas);
+
+// confirm : onaylamak demek , boolen 
+// let sonuç =confirm ("silmek istediğinize emin misiniz?")
+// console.log(sonuç);
---
+++ karş-opr.js
@@ -0,0 +1,15 @@
+/*----------KARŞILAŞTIRMA OPERATÖRLERİ------------ 
+
+== : eşit mi
+!= : eşit değil mi
+> : büyüktür
+<: küçüktür
+>= : büyük eşittir.
+<= : küçük eşittir.
+
+*/
+
+// console.log(5!=3);
+// let yas = 23
+// console.log(yas!=23);
+// console.log(13>=13);
