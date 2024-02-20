3a25e23ed4146651fe8e8c9175822ed823deec70
ayseayse0346 <ayseayse.ayse0346@gmail.com>
2024-02-20 17:59:01 +0300

    array

---
+++ array(diziler)/app.js
@@ -0,0 +1,45 @@
+// syntax
+let arr = ["ayşe","akif","elif","bahar"];
+console.log(arr);
+// burda string değerler tutmuş 
+let dizi = [1,true,"ali","?",null,5.12];
+console.log(dizi);
+console.log(arr[0][2]);
+// burda da hem string hem diğer tipleri de tutmuş yani her tipteki değerleri de tek bir dizi içinde tutabilir.
+
+// ! İNDEX KAVRAMI -->indisleme yapabiliriz. indisleme 0'dan başlar . 
+
+// !örnekler DİZİLERİN İNDEXLERİNİ KULLANARAK İÇİNDEKİ DEĞERLERE ULAŞABİLİRİZ.
+// diziyi tanıladık
+let sayılar= [1,2,3,4,5,6,"ali",7,8,9,"enes"]
+// dizinin içindeki 6 değerini almak istiyorum
+
+console.log(sayılar[5]); //5. indisde altı sayısı olduğu için 5 yazdık inidisler sıfırdan başlar çünkü.
+
+// olmayan indexi istediğimizde hata vermez undefined döner.
+console.log(sayılar[11]);
+
+
+// ! örnek: ali değerini mehmet yap
+
+sayılar[10] ="mehmet"
+// sayılar[sayılar.length-1] = "mehmet" yukarıdakiyle aaynı
+console.log(sayılar);
+
+// ?NOT LENGHT-1 --> EN SONDAKİ ELEMANI GÖSTERİR. LENGHT UZUNLUĞUNU VERİR. KAÇ TANE DEĞERİ VAR ONU SÖYLER.
+
+console.log(sayılar.length);
+
+// !ÖRNEK
+let isimler = ["enes","kübra","bilal","yusuf"]
+console.log(isimler);
+isimler[2] = "yakup"
+console.log(isimler[0]);
+console.log(isimler);
+console.log(isimler.length);
+
+// !örnek:karışık dizi
+
+let karışıkDizi = [1,"enes",5.7,true,null,undefined]
+console.log(karışıkDizi[3]);
+
